0bcbff5a6e55ab90f6f7971bb50839d2629bc960
Akshay-kumar-1234 <ap771316@gmail.com>
2025-09-29 12:30:36 +0530

    Updated server.js with /api/influx route

--- server.js
+++ server.js
@@ -10,15 +10,21 @@ app.use(express.json())
 app.use(cors());
 app.get('/', (req, res) => {
   res.json({ message: "hiii " })
-})
+});
 
-app.use(`/influx`,InfluxRouter)
 
-// const Server=app.listen(3001)  // this already present 
-const PORT = process.env.PORT || 3001; // 
-// done by me fir the app 
+//Api route made by me 
+app.use('/api/influx',InfluxRouter);
+
+
+// // const Server=app.listen(3001)  // this already present 
+ const PORT = process.env.PORT || 3001; 
+
+
+// // done by me for the app 
 const Server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
 
 
